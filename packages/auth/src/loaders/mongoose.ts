427bd7ca72d3fdaf470d6020fc5deafbc659524f
Petr Volf <petr.volf96@gmail.com>
2019-12-17 15:58:26 +0100

    create admin if there are no users in database

--- packages/auth/src/loaders/mongoose.ts
+++ packages/auth/src/loaders/mongoose.ts
@@ -1,7 +1,13 @@
 import mongoose from 'mongoose'
 import config from '../config'
+import AuthModel from '../models/auth'
+import authService from '../services/auth';
 
 export default async () => {
     await mongoose.connect(config.database.url, { useNewUrlParser: true, useCreateIndex: true });
-    //return connection;
+    // if no user is in database, create default admin
+    const usersCount = await AuthModel.count({})
+    if(usersCount === 0) {
+        authService.addUser('admin', 'admin', 'admin')
+    }
 }
